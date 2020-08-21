8a0ff65dc73f5469174849c8d7c459a670c6884f
Sujin <allure1212.c@gmail.com>
2020-08-21 15:29:18 +0900

    1.router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tool-bar></tool-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToolBar from './components/ToolBar.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ToolBar,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.header {
+  color : white;
+  background-color : #42b883;
+  display: flex;
+  padding: 8px;
 }
+.header .router-link-exact-active {
+  color: #35495e;
+}
+.header a {
+  color: white;
+}
+
 </style>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+      <router-link to="/news">News</router-link> |
+      <router-link to="/ask">Ask</router-link> |
+      <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped> 
+
+
+</style>
